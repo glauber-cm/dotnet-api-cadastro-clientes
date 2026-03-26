607a583e42b0d3933519e231da0122513749ad23
Glauber <glauber.charles2012@gmail.com>
2026-03-26 15:49:37 -0300

    inclusão das services de cliente no program.

--- CadastroClientesApi/CadastroClientesApi/Program.cs
+++ CadastroClientesApi/CadastroClientesApi/Program.cs
@@ -1,14 +1,19 @@
 using CadastroClientesApi.Data;
+using CadastroClientesApi.Repositories;
+using CadastroClientesApi.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddScoped<IClienteRepository, ClienteRepository>();
+builder.Services.AddScoped<ClienteService>();
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
 
 
 builder.Services.AddDbContext<AppDbContext>(options =>
